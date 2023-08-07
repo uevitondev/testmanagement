81f441778542c380ac7290573580be295b75ed26
uevitondev <uevitonsantos@outlook.com>
2023-08-07 10:15:12 -0300

    project created

---
+++ testmanagement/src/main/java/com/uevitondev/testmanagement/TestmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.uevitondev.testmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmanagementApplication.class, args);
+	}
+
+}
